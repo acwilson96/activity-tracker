c8359d82b3128cbf4b1c514493994e8927fdcbcc
Alexander Wilson <acwilson96@gmail.com>
2018-07-01 16:47:38 +0100

    Use appropriate port

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app     = express()
 
+const PORT    = process.env.PORT || 5000
+
 app.post('/url_visited', (req, res) => {
     console.log("Received Request");
     res.send('Visited URL: ' + req.data.url);
 });
 
-app.listen(3000, () => console.log('Started acwilson96 analytics on port 3000!'));
+app.listen(PORT, () => console.log('Started acwilson96 analytics on port: ' + PORT));
